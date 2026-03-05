7ffbd234619697f3d2e053e3b6c87a95fa383303
milesc0628 <miles.ec888@gmail.com>
2026-03-05 23:36:05 +0000

    fixed imagegalleryjs

--- jsexamples/js/imagegallery.js
+++ jsexamples/js/imagegallery.js
@@ -1,44 +1,58 @@
-"use strict"
+"use strict";
 
 let galleryClass = document.querySelectorAll(".imageGallery");
 console.log(galleryClass);
 console.log(galleryClass.length);
 
-/*
-galleryClass[0].style.display = "none";
-galleryClass[1].style.display = "none";
-galleryClass[2].style.display = "none";
-*/
+
+// galleryClass[0].style.display = "none";
+// galleryClass[1].style.display = "none";
+// galleryClass[2].style.display = "none";
 /*
 for(let i = 0;i<10;i++){
     console.log(i);
 }
 */
-for(let i = 0; i<galleryClass.length; i++){
+
+function nextImage(){
+    galleryClass[currentImage].style.display = "none";
+    currentImage = currentImage + 1;
+    if (currentImage == galleryClass.length) {
+        currentImage = 0;
+    }
+    galleryClass[currentImage].style.display = "block";
+}
+
+for (let i = 0; i < galleryClass.length; i++) {
     galleryClass[i].style.display = "none";
 }
 
 let currentImage = 0;
+
 galleryClass[currentImage].style.display = "block";
 
 let nextButton = document.getElementById("next");
 
-nextButton.addEventListener("click", function(){
-    galleryClass[currentImage].style.display = "none";
-    currentImage = currentImage + 1;
-    if(currentImage == galleryClass.length){
-        currentImage = 0;
-    }
-    galleryClass[currentImage].style.display = "block";
-});
+// nextButton.addEventListener("click", function () {
+//     // galleryClass[currentImage].style.display = "none";
+//     // currentImage = currentImage + 1;
+//     // if (currentImage == galleryClass.length) {
+//     //     currentImage = 0;
+//     // }
+//     // galleryClass[currentImage].style.display = "block";
+//     nextImage();
+
+// });
+
+nextButton.addEventListener("click", nextImage);
 
 let prevButton = document.getElementById("previous");
 
-prevButton.addEventListener("click", function(){
+prevButton.addEventListener("click", function () {
     galleryClass[currentImage].style.display = "none";
     currentImage = currentImage - 1;
-    if(currentImage == -1){
-        currentImage = galleryClass.length -1;
+    if (currentImage == -1) {
+        currentImage = galleryClass.length - 1;
     }
     galleryClass[currentImage].style.display = "block";
 });
@@ -48,24 +62,24 @@ let startButton = document.getElementById("start");
 let autoCycleActive = false;
 let cycleInterval;
 
-startButton.addEventListener("click", function(){
-    if(autoCycleActive = false){
+startButton.addEventListener("click", function () {
+    if (autoCycleActive == false) {
         autoCycleActive = true;
-        cycleInterval = setInterval(function(){
-        galleryClass[currentImage].style.display = "none";
-    currentImage = currentImage + 1;
-    if(currentImage == galleryClass.length){
-        currentImage = 0;
-    }
-    galleryClass[currentImage].style.display = "block";
-            console.log("Interval started");
-        },3000);
+        cycleInterval = setInterval(function () {
+            console.log("interval started");
+            // galleryClass[currentImage].style.display = "none";
+            // currentImage = currentImage + 1;
+            // if (currentImage == galleryClass.length) {
+            //     currentImage = 0;
+            // }
+            // galleryClass[currentImage].style.display = "block";
+            nextImage();
+        }, 3000);
     }
 });
 
 let stopButton = document.getElementById("stop");
-
-stopButton.addEventListener("click", function() {
+stopButton.addEventListener("click", function(){
     clearInterval(cycleInterval);
     autoCycleActive = false;
 });
